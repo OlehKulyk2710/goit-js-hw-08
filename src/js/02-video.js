af044378b75f40e05dc27d6a5229c3bf6e45a8f6
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-05-07 20:13:45 +0300

    Task-02: Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const currentPlayerTime =
+  localStorage.getItem('videoplayer-current-time') === null
+    ? 0
+    : localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentPlayerTime);
+
+// console.log('currentPlayerTime:', currentPlayerTime);
